Fixed likelihood weighting

main.py
@@ -1,5 +1,7 @@
 import jax
 import optax
+import haiku as hk
+# Make sure to export XLA_PYTHON_CLIENT_PREALLOCATE=false or something similar so that this main process does not eat all GPU memory
 from bsuite.environments import catch
 from replaybuffer import ReplayBuffer
 from learner import Learner
@@ -7,7 +9,6 @@ from models import fSVGDEnsemble
 from nets import CatchNet
 import ray
 from actor import Actor, DQNAgent
-import haiku as hk
 import util
 from parameter_server import ParameterServer
 import time
@@ -16,14 +17,16 @@ import logging
 
 replay_buffer_size = 10000
 short_term_capacity = 20
-batch_size = 4
+batch_size = 64
 learning_rate = 1e-3
 random_seed = 42
 lambda_ = 0.5
 discount_factor = 0.99
-n_networks = 3
+n_networks = 100
 unroll_length = 20
-n_actors = 1
+n_actors = 10
+
+#batch size and n_actors can be used to trade off between actor and learner speed.
 
 def main():
     ray.init()
@@ -75,16 +78,13 @@ def main():
         parameter_server=parameter_server
     ) for i in range(n_actors)]
 
-    print(ray.get(replaybuffer.get_num_frames.remote()))
     learner.run.remote()
     # this is stupid
     while not ray.get(parameter_server.get_params_set.remote()):
         print(time.sleep(0.5))
         print('Waiting for learner..')
     start_params = ray.get(parameter_server.get_params.remote())
-    ray.get([actor.run.remote(100000) for actor in actors])
-    print(ray.get(replaybuffer.get_num_frames.remote()))
-
+    ray.get([actor.run.remote(1000) for actor in actors])
     end_params = ray.get(parameter_server.get_params.remote())
 
     print(jax.tree_multimap(lambda x, y: np.sum((x - y)**2), start_params, end_params))

models.py
@@ -26,29 +26,37 @@ class fSVGDEnsemble():
         self, 
         params: hk.Params, 
         target_params: hk.Params,
-        trajectories: util.Trajectory,
+        batch: Tuple[util.Trajectory, int],
         lambda_: float,
         discount: float,
     ) -> Tuple[jnp.ndarray, Dict[str, jnp.ndarray]]:
         logs = {}
+        trajectories, n_data = batch
         # print(trajectories.observation.shape)
         vapply = jax.vmap(self.ensemble_transformed.apply, in_axes=(None, 0), out_axes=1)
 
         q_tm1 = vapply(params, trajectories)[:, :, :-1]
         # print(q_tm1.shape)
         q_t = vapply(target_params, trajectories)[:, :, 1:]
-        td_loss = multi_step_lambda(q_tm1, q_t, trajectories, lambda_, discount)
+        td_loss = jnp.mean(multi_step_lambda(q_tm1, q_t, trajectories, lambda_, discount))
 
-        Kij = gram_matrix_median_trick(q_tm1)
-        print(Kij.shape)
+        Kij = jax.vmap(jax.vmap(gram_matrix_median_trick, in_axes=1, out_axes=-1), in_axes=1, out_axes=-1)(q_tm1)
+        Kij = jnp.sum(Kij, axis=(-1, -2))
+
         fSVGD_loss = jnp.sum(Kij, axis=1) / jax.lax.stop_gradient(jnp.sum(Kij, axis=1))
 
-        loss = td_loss + fSVGD_loss
+        batch_axes = trajectories.observation.shape[0:2]
+        batch_size = batch_axes[0] * batch_axes[1] 
+        # loss = n_data / batch_size * td_loss + jnp.sum(fSVGD_loss)
+        # logic: likelihood is supposed to scale with n_data, so should have a factor n_data / batch_size
+        # both td loss and fsvgd loss are summed over the batch size, meaning that the factor batch size can just be ignored
+        # and we add 1/n to fsvgd instead of n * td loss to make the loss more manageably small
+        loss = td_loss + 1 / n_data * jnp.mean(fSVGD_loss)
         # logs['q_t'] = q_t
         # logs['q_tm1'] = q_tm1
         logs['td_loss'] = td_loss
-        logs['fSVGD_loss'] = fSVGD_loss
-        return jnp.mean(loss, axis=0), logs
+        # logs['fSVGD_loss'] = fSVGD_loss
+        return loss, logs
 
 @jax.jit
 def multi_step_lambda(q_tm1, q_t, trajectories, lambda_, discount):
@@ -68,7 +76,7 @@ def multi_step_lambda(q_tm1, q_t, trajectories, lambda_, discount):
     )(r_t, discount_t, v_t, lambda_)
     # print(r_t.shape, v_t.shape, target_tm1.shape, q_tm1.shape, q_t.shape)
     action_ohe = jax.nn.one_hot(a_tm1, num_classes=q_tm1.shape[-1])
-    td_loss = jnp.mean( (jax.lax.stop_gradient(target_tm1) - jnp.sum(q_tm1 * action_ohe, axis=-1)) **2)
+    td_loss = jnp.sum( (jax.lax.stop_gradient(target_tm1) - jnp.sum(q_tm1 * action_ohe, axis=-1)) **2, axis=(1, 2))
     return td_loss
 
 # Do I want the batch to disappear in jnp.ravel?

replaybuffer.py
@@ -23,7 +23,7 @@ class ReplayBuffer:
         # stack or concatenate?
         # old_batch = jax.tree_multimap(lambda *x: np.concatenate(x, axis=0), *old_batch)
         # fresh_batch = jax.tree_multimap(lambda *x: np.concatenate(x, axis=0), *fresh_batch)
-        batch = jax.tree_multimap(lambda *x: np.stack(x, axis=0), *(old_batch + fresh_batch))
+        batch = jax.tree_multimap(lambda *x: np.stack(x, axis=0), *(old_batch + fresh_batch)), self.num_frames
         return batch
         
     def get_num_frames(self):